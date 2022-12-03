ac968253c88e7942249ef37809c1311f5381808a
mikelaslam <kipkorir385@gmail.com>
2022-12-03 11:36:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+//write your code here!
+//using the element.remove() to remove the ("main") element
+const element = document.getElementById("main");
+element.remove("main");
+
+//Using document.createElement() to create 'h1#victory'
+let newHeader = document.createElement("h1");
+
+//Creating <h1> with id  'victory'
+ newHeader.id = "victory" 
+
+ //creating <h1> with a message including "YOUR NAME is the champion"
+ newHeader.innerHTML = "YOUR-NAME is the champion"
